3d629cca1044cc2155ea9154f2c3315dd1edf683
Bayram <91245046+Bayram13@users.noreply.github.com>
2025-05-24 20:55:36 +0400

    Update and rename bot.py to main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from threading import Thread
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import os
+
+TOKEN = os.environ.get("TOKEN")
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot aktivdir"
+
+def run():
+    app.run(host='0.0.0.0', port=3000)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+def start(update, context):
+    update.message.reply_text("Salam! Mən aktivəm.")
+
+def salam(update, context):
+    if 'salam' in update.message.text.lower():
+        update.message.reply_text("Aleykum salam")
+
+def main():
+    keep_alive()
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, salam))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
